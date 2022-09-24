ce7816b0c03273a50c8c650299a341ef89aceccb
vishal2376 <vishalsingh2376@gmail.com>
2022-09-24 15:24:00 +0530

    load fragment in MainActivity

--- app/src/main/java/com/vishal/hashgenerator/MainActivity.kt
+++ app/src/main/java/com/vishal/hashgenerator/MainActivity.kt
@@ -2,14 +2,24 @@ package com.vishal.hashgenerator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val successFragment = SuccessFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadFragment(homeFragment)
 
     }
 
+ private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.mainContainer, fragment)
+        transaction.commit()
+    }
 }
