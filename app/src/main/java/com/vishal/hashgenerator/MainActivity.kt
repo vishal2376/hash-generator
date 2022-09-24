ad885279c6d790f7e08a592b165cdb9cb5fe84e8
vishal2376 <vishalsingh2376@gmail.com>
2022-09-24 15:16:19 +0530

    fragment controller removed

--- app/src/main/java/com/vishal/hashgenerator/MainActivity.kt
+++ app/src/main/java/com/vishal/hashgenerator/MainActivity.kt
@@ -1,27 +1,15 @@
 package com.vishal.hashgenerator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //setup fragment nav controller
-        navController = findNavController(R.id.fragment)
-        setupActionBarWithNavController(navController)
 
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
