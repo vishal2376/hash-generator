7980dc24060ee0f7ae469937b57c9d8f174bea97
vishal2376 <vishalsingh2376@gmail.com>
2022-09-24 16:20:50 +0530

    success animation added

--- app/src/main/java/com/vishal/hashgenerator/HomeFragment.kt
+++ app/src/main/java/com/vishal/hashgenerator/HomeFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.*
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.vishal.hashgenerator.databinding.FragmentHomeBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
 
-    private var _binding:FragmentHomeBinding?= null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -23,14 +26,40 @@ class HomeFragment : Fragment() {
         setHasOptionsMenu(true)
 
         val hashAlgorithms = resources.getStringArray(R.array.hash_algorithm)
-        val arrayAdapter = ArrayAdapter(requireContext(),R.layout.drop_down_item,hashAlgorithms)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, hashAlgorithms)
         binding.tvAutoComplete.setAdapter(arrayAdapter)
+
+        //generate button
+        binding.btnGenerate.setOnClickListener {
+            lifecycleScope.launch() {
+                applyAnimations()
+            }
+        }
 
         return binding.root
     }
 
+    private suspend fun applyAnimations() {
+        binding.tvTitle.animate().alpha(0f).duration = 400L
+        binding.btnGenerate.animate().alpha(0f).duration = 400L
+        binding.txtInputLayout.animate()
+            .alpha(0f)
+            .translationXBy(1000f)
+            .duration = 400L
+        binding.etPlainText.animate()
+            .alpha(0f)
+            .translationXBy(-1000f)
+            .duration = 400L
+
+        delay(300)
+
+        binding.viewSuccess.animate().alpha(1f).duration = 400L
+        binding.imgSuccess.animate().alpha(1f).duration = 1000L
+
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu,menu)
+        inflater.inflate(R.menu.menu, menu)
     }
 
     override fun onDestroy() {
